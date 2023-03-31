bae0e97f41d2e6a936e69d9d80d90e2a9e7f16bc
martinniee <1954924950@qq.com>
2023-03-31 09:53:30 +0800

    refactor: Add back to top link function

--- src/options/add-back-to-top-link-for.ts
+++ src/options/add-back-to-top-link-for.ts
@@ -4,52 +4,8 @@ import { getMainContent } from './utils/getMainContent';
 
 
 /**
- * Copy the preccessed contents that have been added back-top-link to clipboard
- * 
- * @param content Text written to clipboard
- */
-
-export const copyContentToClipboard = async (content: string) => {
-
-    try {
-        // Create ClipboardItem object , and write to clipboard
-        await navigator.clipboard.write([
-            new ClipboardItem({ "text/plain": new Blob([content], { type: "text/plain" }) })
-        ]);
-        new Notice("Copy to clipboard!");
-
-    } catch (error) {
-        console.error(error.name, error.message);
-    }
-
-}
-
-/**
- * Get the main contents except the frontmatter
- */
-export const getMainContent = async (): Promise<string> => {
-    const activeMd: TFile = app.workspace.getActiveFile() as TFile;
-    const origin_filecontents = await app.vault.read(activeMd);
-    const fileContents_array = origin_filecontents.split("\n");
-    const new_filecontents: string[] = [];
-
-    let num: number = 0;
-    for (let line of fileContents_array) {
-        const isMatchFrontmatter: boolean = line.match(/^-{3}/g) !== null;
-        if (isMatchFrontmatter && num < 2) {
-            num++
-            continue;
-        }
-        if (num < 2) {
-            continue;
-        }
-        new_filecontents.push(line);
-    }
-    return new_filecontents.join("\n") as string;
-}
-/**
- * 
- * @returns 
+ * --------Add back to top link for juejin--------
+ * @returns Main content of note with added back to top link
  */
 export const addBackToTopLinkForJuejin = async (): Promise<string> => {
     const main_content: string = await getMainContent() as string;
