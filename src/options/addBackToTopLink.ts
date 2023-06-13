1be699fb620261f9e27dfb0e0ccc5f88d97e57cb
martinniee <1954924950@qq.com>
2023-06-14 01:25:12 +0800

    🦄 refactor: 使用 “文件处理工具方法”重构 back2top link的实现

--- src/options/addBackToTopLink.ts
+++ src/options/addBackToTopLink.ts
@@ -1,72 +1,49 @@
-import { TFile } from 'obsidian';
-
+import { fileContentsProcess } from "./utils/utils";
 
 /**
- * 
+ *
  * Add back to top link for other headers
- * 
+ *
  * OtherHeaders: markdown headers excludes h1 header
- * 
+ *
  * example:
- * 		## foo 
+ * 		## foo
  *  -------- ↓ ----------
  * 		[回到顶部](#H1-foo)
- * 		## foo 
- * 
+ * 		## foo
+ *
  * @param docContent current note content
  */
 export const addBackToTopLinkForOtherHeaders = async () => {
-    // get note content
-    const activeMd: TFile = app.workspace.getActiveFile() as TFile;
-    const origin_filecontents = await app.vault.read(activeMd);
-    const new_filecontents: string[] = [];
-    const fileContents_array = origin_filecontents.split("\n");
-
-    // Match h1 header
-    const h1Title = (origin_filecontents.match(/(?<=^# )(.*)/mg) as string[])[0];
-    if (h1Title == null) {
-
-    }
-    //  iterate all lines of current note 
-    for (let line of fileContents_array) {
-        // match h2/3/4/5/6 header
-        const isMatchOtherHeaders: Boolean = line.match(/^#{2,6} +.*/gm) !== null;
-        // if match 
-        if (isMatchOtherHeaders) {
-            const otherHeader = (line.match(/^#{2,6} +.*/gm) as string[])[0];
-            // concat 
-            const afterContent: string = `[回到顶部](#${h1Title})\n${otherHeader}`;
-            new_filecontents.push(line.replace(otherHeader, afterContent));
-        } else {
-            new_filecontents.push(line);
-        }
-    }
-    // write processed content to activeMd
-    app.vault.adapter.write(activeMd.path, new_filecontents.join("\n"));
-
-}
-
+	fileContentsProcess((line: string, metaData: any) => {
+		const h1TitleRegex = /(?<=^# )(?<h1Title>.*)/;
+		let h1Title = "";
+		if (h1TitleRegex.test(line) && !metaData.flag) {
+			metaData.flag = true;
+			const match = h1TitleRegex.exec(line) as RegExpExecArray;
+			h1Title = match?.groups?.h1Title as string;
+			metaData.h1Title = h1Title;
+		}
+		const otherHeadingRegex = /^(?<otherHeader>#{2,6} +.*)/;
+		if (otherHeadingRegex.test(line)) {
+			const otherHeader =
+				otherHeadingRegex.exec(line)?.groups?.otherHeader;
+			// concat
+			const afterContent: string = `[回到顶部](#${metaData.h1Title})\n${otherHeader}`;
+			return line.replace(otherHeader as string, afterContent);
+		}
+		return line;
+	});
+};
 /**
- * Clear existed back-top-link
+ * Clear all existed back-top-links
  */
 export const clearBackToTopLink = async () => {
-    const activeMd: TFile = app.workspace.getActiveFile() as TFile;
-    const origin_filecontents = await app.vault.read(activeMd);
-    const new_filecontents: string[] = [];
-    const fileContents_array = origin_filecontents.split("\n");
-
-    // iterate
-    for (let line of fileContents_array) {
-        //  match [回到顶部](#xxxx)
-        const isMatchBackToTopLink: boolean = line.match(/(\[.*\]\(#.*\))/gm) !== null;
-        // if match link
-        if (isMatchBackToTopLink) {
-            const removeLink = line.replace(/(\[.*\]\(#.*\))/gm, '') as string;
-            new_filecontents.push(removeLink);
-        } else {
-            new_filecontents.push(line);
-        }
-    }
-    // write processed content to activeMd
-    app.vault.adapter.write(activeMd.path, new_filecontents.join("\n"));
-}
+	fileContentsProcess((line: string) => {
+		const back2TopLinkRegex = /(\[回到顶部\]\(#.*\))/;
+		if (back2TopLinkRegex.test(line)) {
+			return line.replace(back2TopLinkRegex, "") as string;
+		}
+		return line;
+	});
+};
--- src/options/utils/utils.ts
+++ src/options/utils/utils.ts
@@ -1,5 +1,8 @@
-import MyPlugin from "main";
 import { TFile } from "obsidian";
+interface metaData {
+	h1Title: string;
+	flag: boolean;
+}
 /**
  *
  * @returns Index where the first h1 heading at
@@ -18,17 +21,17 @@ export const getFirstH1HeadingPostion = async (): Promise<number> => {
 };
 /**
  * File contents processing method
+ * callback: (line:string,plugin?:Plugin,globalVariable?:{})
  * @param callback custom contents processing function
  */
-export const fileContentsProcess = async (
-	callback: any
-) => {
+export const fileContentsProcess = async (callback: any) => {
 	const activeFile: TFile = app.workspace.getActiveFile() as TFile;
 	const fileContents = (await app.vault.read(activeFile)).split("\n");
 	let newFileContents: string[] = [];
+	const metadata = { flag: false };
 	for (let line of fileContents) {
-		newFileContents.push(callback(line));
-    }
-    newFileContents =  newFileContents.filter(item=> item !='DELETED_LINE')
+		newFileContents.push(callback(line, metadata));
+	}
+	newFileContents = newFileContents.filter((item) => item != "DELETED_LINE");
 	app.vault.adapter.write(activeFile.path, newFileContents.join("\n"));
 };
