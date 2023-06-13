31398b7ee5316bbdb63fe3b7b00df0840f070950
martinniee <1954924950@qq.com>
2023-06-14 07:49:48 +0800

    ✨ feat: enhancing function to process async operation

--- src/options/utils/utils.ts
+++ src/options/utils/utils.ts
@@ -19,19 +19,35 @@ export const getFirstH1HeadingPostion = async (): Promise<number> => {
 	}
 	return lineNum;
 };
+
 /**
  * File contents processing method
  * callback: (line:string,plugin?:Plugin,globalVariable?:{})
  * @param callback custom contents processing function
+ * @param isRePromise determing whether return promise
+ * @param delay delay time
+ * @returns
  */
-export const fileContentsProcess = async (callback: any) => {
+export const fileContentsProcess = async (
+	callback: any,
+	isRePromise?: boolean,
+	delay?: number
+): Promise<void> => {
 	const activeFile: TFile = app.workspace.getActiveFile() as TFile;
 	const fileContents = (await app.vault.read(activeFile)).split("\n");
 	let newFileContents: string[] = [];
 	const metadata = { flag: false };
+	if (!delay) delay = 1000; // no delay passed, set 1000 ms as default
 	for (let line of fileContents) {
 		newFileContents.push(callback(line, metadata));
 	}
 	newFileContents = newFileContents.filter((item) => item != "DELETED_LINE");
 	app.vault.adapter.write(activeFile.path, newFileContents.join("\n"));
+	if (isRePromise) {
+		return new Promise((resolve, reject) => {
+			setTimeout(() => {
+				resolve();
+			}, delay);
+		});
+	}
 };
